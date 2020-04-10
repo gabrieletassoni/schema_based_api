a57c5aecc4557fba330c554854243bb26738f7b0
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2020-04-10 22:45:11 +0200

    make AuthController inherit from ActionController::API

--- app/controllers/api/v2/authentication_controller.rb
+++ app/controllers/api/v2/authentication_controller.rb
@@ -1,8 +1,4 @@
-class Api::V2::AuthenticationController < Api::V2::ApplicationController
-    # No authentication for the request, it just needs the username and password
-    skip_before_action :authenticate_request
-    # Here we don't need CanCanCan, no ActiveRecord models here.
-    skip_load_and_authorize_resource
+class Api::V2::AuthenticationController < ActionController::API
     
     def authenticate
         command = AuthenticateUser.call(params[:auth][:email], params[:auth][:password])
