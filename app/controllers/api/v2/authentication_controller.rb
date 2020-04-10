ebffa36d99e4fab5d01a8cc601f09f960a4bc909
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2020-04-10 11:48:10 +0200

    clenaup code

--- app/controllers/api/v2/authentication_controller.rb
+++ app/controllers/api/v2/authentication_controller.rb
@@ -1,6 +1,8 @@
 class Api::V2::AuthenticationController < Api::V2::ApplicationController
-    skip_load_and_authorize_resource
+    # No authentication for the request, it just needs the username and password
     skip_before_action :authenticate_request
+    # Here we don't need CanCanCan, no ActiveRecord models here.
+    skip_load_and_authorize_resource
     
     def authenticate
         command = AuthenticateUser.call(params[:auth][:email], params[:auth][:password])
