f8b963080ab4ff4b5e98adb0f84175484133bc6b
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2020-05-22 10:58:19 +0200

    renames accordingly to new gem name

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,45 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_06_153136) do
+ActiveRecord::Schema.define(version: 2020_05_18_082821) do
+
+  create_table "actions", force: :cascade do |t|
+    t.string "name"
+    t.bigint "lock_version"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_actions_on_name", unique: true
+  end
+
+  create_table "permission_roles", force: :cascade do |t|
+    t.integer "role_id", null: false
+    t.integer "permission_id", null: false
+    t.bigint "lock_version"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["permission_id"], name: "index_permission_roles_on_permission_id"
+    t.index ["role_id"], name: "index_permission_roles_on_role_id"
+  end
+
+  create_table "permissions", force: :cascade do |t|
+    t.integer "predicate_id", null: false
+    t.integer "action_id", null: false
+    t.integer "target_id", null: false
+    t.bigint "lock_version"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["action_id"], name: "index_permissions_on_action_id"
+    t.index ["predicate_id"], name: "index_permissions_on_predicate_id"
+    t.index ["target_id"], name: "index_permissions_on_target_id"
+  end
+
+  create_table "predicates", force: :cascade do |t|
+    t.string "name"
+    t.bigint "lock_version"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_predicates_on_name", unique: true
+  end
 
   create_table "role_users", force: :cascade do |t|
     t.integer "role_id", null: false
@@ -29,14 +67,35 @@ ActiveRecord::Schema.define(version: 2020_03_06_153136) do
     t.index ["name"], name: "index_roles_on_name"
   end
 
+  create_table "targets", force: :cascade do |t|
+    t.string "name"
+    t.bigint "lock_version"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_targets_on_name", unique: true
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.boolean "admin", default: false, null: false
     t.bigint "lock_version"
+    t.boolean "locked", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "permission_roles", "permissions"
+  add_foreign_key "permission_roles", "roles"
+  add_foreign_key "permissions", "actions"
+  add_foreign_key "permissions", "predicates"
+  add_foreign_key "permissions", "targets"
   add_foreign_key "role_users", "roles"
   add_foreign_key "role_users", "users"
 end
